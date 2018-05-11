9611902a8833c0f83e26e58cd9854c4bf8d93fce
ChrisMagnuson <hourglasssand@gmail.com>
2018-05-11 16:20:37 -0400

    Add Install-PowerShellApplication

--- PowerShellApplication.psm1
+++ PowerShellApplication.psm1
@@ -38,3 +38,55 @@ function Uninstall-PowerShellApplicationScheduledTask {
     $RemoteScriptFilePath = ConvertTo-RemotePath -Path $ScriptFilePath -ComputerName $ComputerName
     Remove-Item $RemoteScriptFilePath
 }
+
+function Install-PowerShellApplication {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName,
+        [Parameter(Mandatory)]$ModuleName,
+        $DependentTervisModuleNames,
+        [Parameter(Mandatory)][String]$ScheduledScriptCommandsString,
+        [Parameter(Mandatory)]$ScheduledTasksCredential,
+        [Parameter(Mandatory)]$SchduledTaskName,
+        [Parameter(Mandatory)]
+        [ValidateScript({ $_ | Get-RepetitionInterval })]
+        $RepetitionIntervalName
+    )
+    process {
+        $ProgramData = Invoke-Command -ComputerName $ComputerName -ScriptBlock { $env:ProgramData }
+        $DirectoryLocal = "$ProgramData\PowerShellApplication\$ModuleName"
+        $DirectoryRemote = $DirectoryLocal | ConvertTo-RemotePath -ComputerName $ComputerName
+        Remove-Item -Path $DirectoryRemote -ErrorAction SilentlyContinue -Recurse -Force
+        New-Item -ItemType Directory -Path $DirectoryRemote -ErrorAction SilentlyContinue
+
+        $PSDependInputObject =  @{
+            PSDependOptions = @{
+                Target = $DirectoryRemote
+            }
+        }
+        (@($ModuleName) + $DependentTervisModuleNames) |
+        ForEach-Object { 
+            $PSDependInputObject.Add( "Tervis-Tumbler/$_", "master") 
+        }
+        
+        Invoke-PSDepend -Force -Install -InputObject $PSDependInputObject
+        $OFSBackup = $OFS
+        $OFS = ""
+@"
+Get-ChildItem -Path $DirectoryLocal -Directory | 
+ForEach-Object {
+    Import-Module -Name `$_.FullName -Force
+}
+
+$ScheduledScriptCommandsString
+"@ |
+        Out-File -FilePath $DirectoryRemote\Script.ps1
+        
+        $OFS = $OFSBackup
+
+        Install-PowerShellApplicationScheduledTask -PathToScriptForScheduledTask $DirectoryLocal\Script.ps1 `
+            -TaskName $SchduledTaskName `
+            -Credential $ScheduledTasksCredential `
+            -RepetitionInterval $RepetitionInterval `
+            -ComputerName $ComputerName
+    }
+}
